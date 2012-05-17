fe4cfc35afd9f55e028013b125aa5d2a38237a0a
Venelin Valkov <venelin@naughtyspirit.com>
2012-05-17 15:31:47 +0300

    Added examples of Lua classes

--- documentation/code/coroutines.lua
+++ documentation/code/coroutines.lua
@@ -1,18 +1,18 @@
 function foo (a)
-       print("foo", a)
-       return coroutine.yield(2*a)
- end
- 
- co = coroutine.create(function (a,b)
-	   print("co-body", a, b)
-	   local r = foo(a+1)
-	   print("co-body", r)
-	   local r, s = coroutine.yield(a+b, a-b)
-	   print("co-body", r, s)
-	   return b, "end"
- end)
+	print("foo", a)
+	return coroutine.yield(2*a)
+end
+
+myCoroutine = coroutine.create(function (a,b)
+	print("myCoroutine-body", a, b)
+	local r = foo(a+1)
+	print("myCoroutine-body", r)
+	local r, s = coroutine.yield(a+b, a-b)
+	print("myCoroutine-body", r, s)
+	return b, "end"
+end)
      
- print("main", coroutine.resume(co, 1, 10))
- print("main", coroutine.resume(co, "r"))
- print("main", coroutine.resume(co, "x", "y"))
- print("main", coroutine.resume(co, "x", "y"))
+print("main", coroutine.resume(myCoroutine, 1, 10))
+print("main", coroutine.resume(myCoroutine, "r"))
+print("main", coroutine.resume(myCoroutine, "x", "y"))
+print("main", coroutine.resume(myCoroutine, "x", "y"))
